2eb57a2b13340bd419e3278fa042f343bd061594
Sören Beye <github@hypfer.de>
2026-05-07 21:20:52 +0200

    Revert "feat: Vibecoded Marstek Meter emulator"
    This reverts commit df783509cf0ce74281ef4ec850809d5c00236cc8.

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const Logger = require("./Logger");
 const Poller = require("./Poller");
 const MqttClient = require("./MqttClient");
 const MarstekShellyEmu = require("./MarstekShellyEmu");
-const MarstekCtEmu = require("./MarstekCt002Emu");
-const MqttBatteryState = require("./MqttBatteryState");
 
 if (process.env.LOGLEVEL) {
     Logger.setLogLevel(process.env.LOGLEVEL);
@@ -11,39 +9,22 @@ if (process.env.LOGLEVEL) {
 
 const poller = new Poller();
 let marstekShellyEmu = null;
-let marstekCtEmu = null;
 
-const ctType = (process.env.CT_TYPE || "").toLowerCase();
-if (ctType === "shelly") {
+if (process.env.MARSTEK_SHELLY_EMU) {
     Logger.info("Initializing MarstekShellyEmu");
+
     marstekShellyEmu = new MarstekShellyEmu();
     marstekShellyEmu.initialize();
-} else if (ctType === "hme-3" || ctType === "hme-4") {
-    Logger.info(`Initializing MarstekCtEmu (${process.env.CT_TYPE})`);
-    marstekCtEmu = new MarstekCtEmu();
-    marstekCtEmu.initialize();
+
+    poller.onData((data) => {
+        marstekShellyEmu.updateMeterReading({
+            total: data.W_TOTAL,
+            l1: data.W_L1,
+            l2: data.W_L2,
+            l3: data.W_L3
+        });
+    });
 }
-
-// Initialize MQTT battery state tracking
-const mqttBatteryState = new MqttBatteryState();
-mqttBatteryState.initialize();
-
-// Wire MQTT battery state into CT emulator steering algorithm
-if (marstekCtEmu) {
-    marstekCtEmu.steering.mqtt = mqttBatteryState;
-}
-
-poller.onData((data) => {
-    const powerData = {
-        total: data.W_TOTAL,
-        l1: data.W_L1,
-        l2: data.W_L2,
-        l3: data.W_L3
-    };
-
-    if (marstekShellyEmu) marstekShellyEmu.updateMeterReading(powerData);
-    if (marstekCtEmu) marstekCtEmu.updateMeterReading(powerData);
-});
 
 const mqttClient = new MqttClient(poller, marstekShellyEmu);
 
@@ -53,3 +34,4 @@ poller.initialize().then(() => {
     Logger.error("Error while initializing poller", err);
     process.exit(1);
 });
+
